e0f1ed78b9edb7980d44c1a9678758c6596fbf8c
klhaim7 <klhaim7@gmail.com>
2020-03-23 22:07:46 +0200

    some changes

--- src/main/java/com/example/microservices/limitsservice/LimitsServiceApplication.java
+++ src/main/java/com/example/microservices/limitsservice/LimitsServiceApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
+@EnableDiscoveryClient
 public class LimitsServiceApplication {
 
     public static void main(String[] args) {
